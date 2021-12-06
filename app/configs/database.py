e6c0f461572e70df4bf4ee6f279b02d7425caf41
douglas filipe <douglasfelipe.net77@gmail.com>
2021-12-06 19:03:39 -0400

    feat: add model prizes

--- app/configs/database.py
+++ app/configs/database.py
@@ -4,8 +4,8 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
 def init_app(app: Flask):
     db.init_app(app)
+    from app.models.prize_model import PrizeModel
     app.db = db
-
-    
