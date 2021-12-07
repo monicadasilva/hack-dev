0a7802af28b6bd27685f061886984a17ce58cac4
douglas filipe <douglasfelipe.net77@gmail.com>
2021-12-07 19:50:11 -0400

    feat: add feature user delete

--- app/controllers/user_controller.py
+++ app/controllers/user_controller.py
@@ -5,13 +5,15 @@ from app.models.avatar_model import AvatarModel
 from app.models.users_model import UserModel
 from flask_jwt_extended import create_access_token
 from sqlalchemy import exc
+from werkzeug.exceptions import NotFound
 
 
 def login():
     try:
         data = request.get_json()
 
-        user: UserModel = UserModel.query.filter_by(email=data["email"]).first()
+        user: UserModel = UserModel.query.filter_by(
+            email=data["email"]).first()
 
         if user.verify_password(data["password"]):
             token = create_access_token(user)
@@ -37,32 +39,58 @@ def login():
 def create_user():
     session = current_app.db.session
     data = request.get_json()
-    
+
     try:
         user = UserModel(**data)
         session.add(user)
         session.commit()
-        
-        return {'id':user.id, 'name': user.name, 'password':user.password_hash, 'points': user.points}, 201
+
+        return {'id': user.id, 'name': user.name, 'password': user.password_hash, 'points': user.points}, 201
 
     except exc.IntegrityError:
-            return {'msg': 'This email already registered!'}, 409
-
+        return {'msg': 'This email already registered!'}, 409
 
 
 def update_avatar(id):
     session = current_app.db.session
     user_avatar = request.files['avatar']
-    
-    
+
     filename = secure_filename(user_avatar.filename)
-    
+
     img = AvatarModel(data=user_avatar.read(), name=filename)
     session.add(img)
     session.commit()
-    
-    
+
     UserModel.query.filter_by(id=id).update({'avatar_id': img.id})
     session.commit()
 
     return '', 204
+
+
+def user_info(id):
+    try:
+        session = current_app.db.session
+        user = UserModel.query.filter_by(id=id).first_or_404()
+
+        session.commit()
+
+        return jsonify({
+            "id": user.id,
+            "name": user.name,
+            "email": user.email,
+        }), 200
+    except NotFound:
+        return {"error": "User not found"}, 404
+
+
+def delete_user(id):
+    try:
+        session = current_app.db.session
+        query = UserModel.query.filter_by(id=id).first_or_404()
+    
+        session.delete(query)
+        session.commit()
+
+        return "ok", 204
+    except NotFound:
+        return {"error": "User not found"}, 404
--- app/routes/user_blueprint.py
+++ app/routes/user_blueprint.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
-from app.controllers.user_controller import create_user, login, update_avatar
+from app.controllers.user_controller import create_user, delete_user, login, update_avatar, user_info
 
 bp = Blueprint("bp_user", __name__, url_prefix="/users")
 
@@ -7,3 +7,5 @@ bp.post("/login")(login)
 
 bp.post("/signup")(create_user)
 bp.patch("/update/<int:id>")(update_avatar)
+bp.get("/<int:id>")(user_info)
+bp.delete("/<int:id>")(delete_user)
