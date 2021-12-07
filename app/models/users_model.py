e7fd627836b9e61bf02f8c673666390daa82fa74
Estevan T. D. Martins <estevandias7@gmail.com>
2021-12-07 17:49:51 -0300

    fix: setter function replacing password attribute, password attribute renamed to password_hash;

--- app/models/users_model.py
+++ app/models/users_model.py
@@ -19,7 +19,7 @@ class UserModel(db.Model):
     id = Column(Integer, primary_key=True)
     name = Column(String(100), nullable=False, unique=True)
     email = Column(String, nullable=False, unique=True)
-    password = Column(String, nullable=True)
+    password_hash = Column(String, nullable=True)
     points = Column(Integer, nullable=False, default=0)
     avatar = Column(String, nullable=False)
     address_id = Column(Integer, ForeignKey("user_address.id"))
@@ -29,15 +29,14 @@ class UserModel(db.Model):
     events = relationship("EventsModel", backref=backref("users"), uselist=False)
 
     address = db.relationship('AddressModel', backref=backref('users'), uselist=False)
-    
+
     @property
     def password(self):
         raise AttributeError("Password not found!")
 
     @password.setter
-    def password_to_hash(self, password):
-        self.password = generate_password_hash(password)
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
 
     def verify_password(self, password):
-        return check_password_hash(self.password, password)
-
+        return check_password_hash(self.password_hash, password)
