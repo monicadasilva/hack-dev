b2d28326abf9dc3c05194cb793734662df41a74d
Estevan T. D. Martins <estevandias7@gmail.com>
2021-12-07 17:52:51 -0300

    feat: jwt configured;

--- app/configs/configs.py
+++ app/configs/configs.py
@@ -6,7 +6,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-def init_app(app:Flask):
+def init_app(app: Flask):
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = bool(os.environ.get('SQLALCHEMY_TRACK_MODIFICATIONS'))
     app.config["JSON_SORT_KEYS"] = False
+    app.config['JWT_SECRET_KEY'] = os.environ.get('SECRET_KEY')
