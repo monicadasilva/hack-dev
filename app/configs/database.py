c66c135541079c6121d2a1979327cc093a9295eb
Mônica S <monica.s.bergues@gmail.com>
2021-12-06 21:12:10 -0300

    feat: added address model

--- app/configs/database.py
+++ app/configs/database.py
@@ -8,4 +8,5 @@ db = SQLAlchemy()
 def init_app(app: Flask):
     db.init_app(app)
     from app.models.prize_model import PrizeModel
+    from app.models.address_model import AddressModel
     app.db = db
