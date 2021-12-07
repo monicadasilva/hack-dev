2e2277a0f6b510bd1ba9da20b7a0ea395f59a74f
Mônica S <monica.s.bergues@gmail.com>
2021-12-07 11:03:14 -0300

    feat: added admin model

---
+++ app/models/admin_model.py
@@ -0,0 +1,33 @@
+from app.configs.database import db
+from sqlalchemy import Column, Integer, String
+from werkzeug.security import generate_password_hash, check_password_hash
+from dataclasses import dataclass
+
+
+@dataclass
+class AdminModel(db.Model):
+    id: int
+    name: str
+    email: str
+    password: str
+
+
+    __tablename__ = "admin"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100), nullable=False, unique=True)
+    email = Column(String, nullable=False, unique=True)
+    password = Column(String, nullable=True)
+    avatar = Column(String, nullable=False)
+   
+
+    @property
+    def password(self):
+        raise AttributeError("Password not found!")
+
+    @password.setter
+    def password_to_hash(self, password):
+        self.password = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password, password)
