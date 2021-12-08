05e145a5209c66479b12de0bf86e07970f8a4780
Estevan T. D. Martins <estevandias7@gmail.com>
2021-12-08 16:43:14 -0300

    feat: create table company;

---
+++ migrations/versions/241fe1c3958a_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 241fe1c3958a
+Revises: 22ae1cea3d9e, d09b55c51d81
+Create Date: 2021-12-08 16:35:20.487409
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '241fe1c3958a'
+down_revision = ('22ae1cea3d9e', 'd09b55c51d81')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
