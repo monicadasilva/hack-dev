35d38a0ee4f3b10d936863293a38eedc4f678d5c
douglas filipe <douglasfelipe.net77@gmail.com>
2021-12-09 17:02:23 -0400

    make it better

---
+++ app/configs/swagger.py
@@ -0,0 +1,22 @@
+template = {
+  "swagger": "2.0",
+  "info": {
+    "title": "My API",
+    "description": "API for my data",
+    "contact": {
+      "responsibleOrganization": "ME",
+      "responsibleDeveloper": "Me",
+      "email": "me@me.com",
+      "url": "www.me.com",
+    },
+    "termsOfService": "http://me.com/terms",
+    "version": "0.0.1"
+  },
+  "host": "mysite.com",  # overrides localhost:500
+  "basePath": "/api",  # base bash for blueprint registration
+  "schemes": [
+    "http",
+    "https"
+  ],
+  "operationId": "getmyData"
+}
