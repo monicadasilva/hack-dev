656b5cdd143d61ac802e658485a9dcb40e18e0a3
Estevan T. D. Martins <estevandias7@gmail.com>
2021-12-14 12:04:08 -0300

    feat: relationship between user and group created without creating a foreign key in the user table;

---
+++ app/models/user_group_table.py
@@ -0,0 +1,8 @@
+from app.configs.database import db
+from sqlalchemy import Column, Integer, ForeignKey
+
+user_group = db.Table(
+    "event_group",
+    Column("id", Integer, primary_key=True),
+    Column("user_id", Integer, ForeignKey("users.id"))
+)
