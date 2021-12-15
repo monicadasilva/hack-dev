95d5a657c2a8fbb0ea544bb9742bafbbf2a5c43a
Mônica S <monica.s.bergues@gmail.com>
2021-12-15 11:04:23 -0300

    feat: added routes to company login/signup with google account

--- app/routes/company_blueprint.py
+++ app/routes/company_blueprint.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
-from app.controllers.company_controller import create_company, create_event, get_all_companies, get_one_company, login_company
+from app.controllers.company_controller import authorize, create_company, create_event, get_all_companies, get_one_company, google_login, login_company, logout
 from flask_cors import CORS
 
 bp_company = Blueprint("bp_company", __name__, url_prefix="/company")
@@ -11,3 +11,6 @@ bp_company.post("/login")(login_company)
 bp_company.post("/events")(create_event)
 bp_company.get("/<int:id>")(get_one_company)
 bp_company.get("")(get_all_companies)
+bp_company.get("/login/google")(google_login)
+bp_company.get("/authorize")(authorize)
+bp_company.get("/logout")(logout)
