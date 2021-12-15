c5aca1dbdbe019a78ac4cecb73a4fc4646595196
douglas filipe <douglasfelipe.net77@gmail.com>
2021-12-15 13:37:49 -0400

    feat: add route users event

--- app/routes/event_blueprint.py
+++ app/routes/event_blueprint.py
@@ -1,11 +1,13 @@
 from flask import Blueprint
 
 
-from app.controllers.event_controller import details_event, read_events
+from app.controllers.event_controller import details_event, pdf_event, read_events, users_event
 
 bp_event = Blueprint("bp_event", __name__, url_prefix="/events")
 
 
 
 bp_event.get("/<int:id>")(details_event)
+bp_event.get("/pdf/<int:id>")(pdf_event)
+bp_event.get("/users/<int:id>")(users_event)
 bp_event.get("")(read_events)
