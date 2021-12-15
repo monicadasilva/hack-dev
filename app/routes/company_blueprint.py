5eace149958e18b618f10307d5f98bcf3e884d8d
douglas filipe <douglasfelipe.net77@gmail.com>
2021-12-15 09:54:16 -0400

    feat: add recuperate password company

--- app/routes/company_blueprint.py
+++ app/routes/company_blueprint.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
-from app.controllers.company_controller import create_company, create_event, get_all_companies, get_one_company, login_company
+from app.controllers.company_controller import create_company, create_event, get_all_companies, get_one_company, login_company, recuperate_password
 
 
 bp_company = Blueprint("bp_company", __name__, url_prefix="/company")
@@ -11,3 +11,4 @@ bp_company.post("/login")(login_company)
 bp_company.post("/events")(create_event)
 bp_company.get("/<int:id>")(get_one_company)
 bp_company.get("")(get_all_companies)
+bp_company.patch("/recuperate-password")(recuperate_password)
